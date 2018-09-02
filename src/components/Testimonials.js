5efe17bcd75d648de0fe8f87bf156e726791959e
Piotr Lasota <lasota.piotr1@gmail.com>
2018-09-02 20:02:23 +0200

    Update testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,6 +4,7 @@ import { between } from 'polished'
 import media from '../styleUtils/media'
 import Blockquote from './Blockquote'
 import Container from './Container'
+import LinkFeature from './LinkFeature'
 
 const Testimonials = () => {
   return (
@@ -11,11 +12,19 @@ const Testimonials = () => {
       <Container>
         <Testimonial>
           <TestimonialText>
-            “We’ve been using Stack to prototype designs quickly and
-            efficiently. Needless to say we’re hugely impressed by the style and
-            value.”
+            "Panią Ilonę cechuje profesjonalne podejście do pacjenta. Pomaga w
+            rozwiązywaniu problemów i "wyjściu na prostą". W gabinecie panuje
+            miła i sympatyczna atmosfera, co powoduje, że chętnie się tam
+            wraca."
           </TestimonialText>
-          <TestimonialFooter>Maguerite Holland</TestimonialFooter>
+          <TestimonialFooter>Lidia</TestimonialFooter>
+
+          <TestimonialFooterAside>
+            Źródło:{' '}
+            <LinkFeature to="https://www.znanylekarz.pl/ilona-lasota/psycholog-psychoterapeuta/warszawa">
+              znanylekarz.pl
+            </LinkFeature>
+          </TestimonialFooterAside>
         </Testimonial>
       </Container>
     </TestimonialsWrapper>
@@ -25,6 +34,7 @@ const Testimonials = () => {
 const Testimonial = styled.div`
   margin: 0 auto;
   max-width: 66.66%;
+  text-align: center;
   ${media.tablet`
     max-width: 83.33%;
   `};
@@ -51,6 +61,12 @@ const TestimonialFooter = styled.span`
   font-weight: 500;
   text-align: center;
   display: block;
+  margin-bottom: 1.5rem;
+`
+
+const TestimonialFooterAside = styled.span`
+  display: block;
+  font-size: calc(10.64px + 0.11vw);
 `
 
 export default Testimonials
