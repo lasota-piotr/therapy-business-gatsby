f1d50afdcf8ba6c02a38890a957662ee0443ea44
Piotr Lasota <lasota.piotr1@gmail.com>
2018-09-13 22:37:53 +0200

    add trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-125761349-1',
       },
     },
     {
